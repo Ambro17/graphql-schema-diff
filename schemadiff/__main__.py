8536c3157c2f8ef0360e613e6ee2131c3339b64f
Nahuel Ambrosini <nambrosini@onapsis.com>
2019-12-25 17:22:47 -0300

    Improve cli format output

--- schemadiff/__main__.py
+++ schemadiff/__main__.py
@@ -1,7 +1,9 @@
 import sys
 import argparse
 
-from schemadiff.compare import SchemaComparator, Schema
+from schemadiff.changes import Change, Criticality
+from schemadiff.diff.schema import Schema
+from schemadiff.graphql_schema import GraphQLSchema
 
 
 def parse_args():
@@ -19,14 +21,35 @@ def parse_args():
     return parser.parse_args()
 
 
+def format_change_by_criticality(change: Change) -> str:
+    icon_by_criticality = {
+        Criticality.Breaking: '❌',
+        Criticality.Dangerous: '⚠️',
+        Criticality.NonBreaking: '✅',
+    }
+    icon = icon_by_criticality[change.criticality.level]
+    return f"{icon} {change.message}"
+
+
+def format_diff(changes: [Change]) -> str:
+    changes = '\n'.join(
+        format_change_by_criticality(change)
+        for change in changes
+    )
+    return changes or '🎉 Both schemas are equal!'
+
+
 def main():
     args = parse_args()
-    old_schema = Schema.from_sdl(args.old_schema.read())
-    new_schema = Schema.from_sdl(args.new_schema.read())
+
+    # Load schemas from file path args
+    old_schema = GraphQLSchema.from_sdl(args.old_schema.read())
+    new_schema = GraphQLSchema.from_sdl(args.new_schema.read())
     args.old_schema.close()
     args.new_schema.close()
 
-    diff = SchemaComparator(old_schema, new_schema).compare()
+    diff = Schema(old_schema, new_schema).diff()
+    print(format_diff(diff))
 
 
 if __name__ == '__main__':
