39e26cdf5315cb0225fb50782cee972291aa2846
Nahuel Ambrosini <nambrosini@onapsis.com>
2019-12-26 09:26:33 -0300

    Pin versions to solve mutual dependencies over attrs between my project and pytest

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ package_name = "graphql-schema-diff"
 
 setup(
     name=package_name,
-    version="0.0.1",
+    version="0.0.3",
     author="Nahuel Ambrosini",
     author_email="ambro17.1@gmail.com",
     description="Compare GraphQL Schemas",
