9b0bb12308d475811ed4e57aa2dc9ae18a471e6e
Ambro17 <ambro17.1@gmail.com>
2020-03-29 19:14:03 -0300

    Add schema level changes

---
+++ schemadiff/changes/schema.py
@@ -0,0 +1,33 @@
+from schemadiff.changes import Change, Criticality
+
+class SchemaChange:
+    criticality: Criticality.breaking('Changing a root type is a breaking change')
+
+    def __init__(self, old_type, new_type):
+        self.old_type = old_type
+        self.new_type = new_type
+
+    @property
+    def path(self):
+        return self.new_type
+
+
+class SchemaQueryTypeChanged(SchemaChange):
+
+    @property
+    def message(self):
+        return f"Schema query root has changed from `{self.old_type}` to `{self.new_type}`"
+
+
+class SchemaMutationTypeChanged(SchemaChange):
+
+    @property
+    def message(self):
+        return f"Schema mutation root has changed from `{self.old_type}` to `{self.new_type}`"
+
+
+class SchemaSubscriptionTypeChanged(SchemaChange):
+
+    @property
+    def message(self):
+        return f"Schema subscription root has changed from `{self.old_type}` to `{self.new_type}`"
