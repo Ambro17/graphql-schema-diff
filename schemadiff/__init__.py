7928a1efa8124039efd1f0b8bf39f5fa456e3dd3
Ambro17 <ambro17.1@gmail.com>
2020-04-18 12:22:44 -0300

    Add Change as top level import as it is a core interface. Also improve type hints and add docstring for better docs

--- schemadiff/__init__.py
+++ schemadiff/__init__.py
@@ -1,21 +1,24 @@
-from typing import Union
+from typing import Union, List
 
 from graphql import GraphQLSchema as GQLSchema, is_schema
 
 from schemadiff.changes import Change
 from schemadiff.diff.schema import Schema
-from schemadiff.graphql_schema import GraphQLSchema
+from schemadiff.graphql_schema import Schema
 from schemadiff.formatting import print_diff, format_diff
 
 
-def diff(schema_a: Union[str, GQLSchema], schema_b: Union[str, GQLSchema]) -> [Change]:
-    """Compare two graphql schemas SDLs highlighting dangerous and breaking changes.
+SDL = str  # Alias for string describing schema through schema definition language
+
+
+def diff(old_schema: Union[SDL, GQLSchema], new_schema: Union[SDL, GQLSchema]) -> List[Change]:
+    """Compare two graphql schemas highlighting dangerous and breaking changes.
 
     Returns:
-        changes (list<Change>): List of differences between both schemas with details about each change
+        changes (List[Change]): List of differences between both schemas with details about each change
     """
-    first = GraphQLSchema.from_sdl(schema_a) if not is_schema(schema_a) else schema_a
-    second = GraphQLSchema.from_sdl(schema_b) if not is_schema(schema_b) else schema_b
+    first = Schema.from_sdl(old_schema) if not is_schema(old_schema) else old_schema
+    second = Schema.from_sdl(new_schema) if not is_schema(new_schema) else new_schema
     return Schema(first, second).diff()
 
 
@@ -23,10 +26,10 @@ def diff_from_file(schema_file: str, other_schema_file: str):
     """Compare two graphql schema files highlighting dangerous and breaking changes.
 
     Returns:
-        changes (list<Change>): List of differences between both schemas with details about each change
+        changes (List[Change]): List of differences between both schemas with details about each change
     """
-    first = GraphQLSchema.from_file(schema_file)
-    second = GraphQLSchema.from_file(other_schema_file)
+    first = Schema.from_file(schema_file)
+    second = Schema.from_file(other_schema_file)
     return Schema(first, second).diff()
 
 
@@ -35,5 +38,6 @@ __all__ = [
     'diff_from_file',
     'format_diff',
     'print_diff',
-    'format_diff'
+    'format_diff',
+    'Change',
 ]
