a3ffd57fb609e749d2f6d7844cd9a4ad162e26da
Ambro17 <ambro17.1@gmail.com>
2020-09-06 14:06:49 -0300

    🚀 Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ package_name = "graphql-schema-diff"
 
 setup(
     name=package_name,
-    version="1.0.4",
+    version="1.0.5",
     author="Nahuel Ambrosini",
     author_email="ambro17.1@gmail.com",
     description="Compare GraphQL Schemas",
